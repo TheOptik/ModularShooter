ad8e23571615119908a19b06d9998be5972c4386
Simon Hümmer <simon.huemmer@huemmer-satz.de>
2015-12-16 10:45:16 +0100

    Probably the last refactoring for now. Lets do at least some working

--- ModularShooter/src/objects/Protagonist.java
+++ ModularShooter/src/objects/Protagonist.java
@@ -19,7 +19,7 @@ public class Protagonist extends GameObject implements Tickable, Drawable {
 
 	private final EventHandler<KeyEvent> keyPressed;
 	private final EventHandler<KeyEvent> keyReleased;
-	private static final List<Module> MODULES = new ArrayList<Module>();
+	private static final List<Module> MODULES = new ArrayList<>();
 
 	private boolean right;
 	private boolean left;
@@ -27,57 +27,11 @@ public class Protagonist extends GameObject implements Tickable, Drawable {
 	private boolean down;
 
 	public Protagonist() {
-		this.coordinates = new Coordinates((World.WIDTH / 2), (World.HEIGHT / 2));
+		this.coordinates = new Coordinates(World.WIDTH / 2, World.HEIGHT / 2);
 		this.velocity = new Velocity(0, 0);
 
-		keyPressed = new EventHandler<KeyEvent>() {
-
-			@Override
-			public void handle(final KeyEvent event) {
-				if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.W) {
-					Protagonist.this.velocity.yVelocity = -1;
-					up = true;
-				}
-				if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
-					Protagonist.this.velocity.yVelocity = 1;
-					down = true;
-				}
-				if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
-					Protagonist.this.velocity.xVelocity = -1;
-					left = true;
-				}
-				if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D) {
-					Protagonist.this.velocity.xVelocity = 1;
-					right = true;
-				}
-				if (event.getCode() == KeyCode.SPACE) {
-					System.out.println("PEW!!");
-				}
-			}
-
-		};
-		keyReleased = new EventHandler<KeyEvent>() {
-
-			@Override
-			public void handle(KeyEvent event) {
-				if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.W) {
-					Protagonist.this.velocity.yVelocity *= 0.9;
-					up = false;
-				}
-				if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
-					Protagonist.this.velocity.yVelocity *= 0.9;
-					down = false;
-				}
-				if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
-					Protagonist.this.velocity.xVelocity *= 0.9;
-					left = false;
-				}
-				if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D) {
-					Protagonist.this.velocity.xVelocity *= 0.9;
-					right = false;
-				}
-			}
-		};
+		keyPressed = generateKeyPressedEventhandler();
+		keyReleased = generateKeyReleasedEventHandler();
 
 	}
 
@@ -132,6 +86,60 @@ public class Protagonist extends GameObject implements Tickable, Drawable {
 
 	public void addModule(Module module) {
 		Protagonist.MODULES.add(module);
+	}
+
+	private EventHandler<KeyEvent> generateKeyPressedEventhandler() {
+		return new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(final KeyEvent event) {
+				if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.W) {
+					Protagonist.this.velocity.yVelocity = -1;
+					up = true;
+				}
+				if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
+					Protagonist.this.velocity.yVelocity = 1;
+					down = true;
+				}
+				if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
+					Protagonist.this.velocity.xVelocity = -1;
+					left = true;
+				}
+				if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D) {
+					Protagonist.this.velocity.xVelocity = 1;
+					right = true;
+				}
+				if (event.getCode() == KeyCode.SPACE) {
+					System.out.println("PEW!!");
+				}
+			}
+
+		};
+	}
+
+	private EventHandler<KeyEvent> generateKeyReleasedEventHandler() {
+		return new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.UP || event.getCode() == KeyCode.W) {
+					Protagonist.this.velocity.yVelocity *= 0.9;
+					up = false;
+				}
+				if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S) {
+					Protagonist.this.velocity.yVelocity *= 0.9;
+					down = false;
+				}
+				if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
+					Protagonist.this.velocity.xVelocity *= 0.9;
+					left = false;
+				}
+				if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D) {
+					Protagonist.this.velocity.xVelocity *= 0.9;
+					right = false;
+				}
+			}
+		};
 	}
 
 }
